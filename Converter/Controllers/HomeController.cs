deafce501a6c78daf1bfdb00ce7997459afe01a9
mcalus3 <marek.calus3@gmail.com>
2017-09-15 09:28:33 +0200

    Simple MVC site for converting created.

---
+++ Converter/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Converter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Converter.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index(string plainText, string type)
+        {
+            var vm = new IndexViewModel();
+            vm.PlainText = plainText;
+
+            if (!string.IsNullOrEmpty(type))
+            {
+                SortedDocument doc = SortedDocumentFactory.Instance.CreateSortedDocument(type, plainText);
+                vm.ConvertedDocument = doc.GetFormattedString();
+            }
+
+            return View(vm);
+        }
+        //
+        // GET: Home/ApiDoc/
+        public ActionResult ApiDoc()
+        {
+            return View();
+        }
+    }
+}
---
+++ Converter/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Converter.Models
+{
+    public class IndexViewModel
+    {
+        public string PlainText { get; set; }
+        public string ConvertedDocument { get; set; }
+        public string Type { get; set; }
+    }
+}
